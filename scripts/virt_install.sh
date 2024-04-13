17c9e9bf1bad3676d21add072a363f9793e733ec
Albert Xu <albertxu@freelamp.com>
2024-04-13 21:00:54 +0800

    fix img size default 10g which is too small for cloud image

--- scripts/virt_install.sh
+++ scripts/virt_install.sh
@@ -18,7 +18,14 @@
 # ============================================================================
 # VM Name
 NAME=$1
-[ -n "$(virsh -q list --name --all | grep -i $NAME)" ] && echo "Error: domain $NAME already exist!" && exit 1
+LC_ALL=C
+if virsh -q dominfo "$NAME" 1>/dev/null 2>&1; then
+	echo "Error: domain $NAME already exist!"
+	exit 1
+else
+	echo "Info: start to create domain $NAME ..."
+fi
+
 # IMG can be local path for iso/qcow2, can also be OS-Type(see supported_os)
 IMG=$2
 # 2c/4GB/10GB as default
@@ -27,18 +34,20 @@ MEM=4096
 DISK_SIZE=$3
 [ -z "$DISK_SIZE" ] && DISK_SIZE=10
 [ -z "$IMG" ] && echo "Syntax: $0 vm_name image_file [size] or $0 vm_name vm_type/img_url" && exit 0
-# Cloud Image dir, Do not place "-" in dir name
+# Cloud Image dir, ** Do not place "-" in dir name!!! **
 BASE_DIR="/nfsroot/iso/cloud"
 # Output VM Dir
 VM_DIR="/nfsroot/VMs/cloud"
 # Your own key file to load into cloud image
 PUB_KEY="$HOME/.ssh/id_rsa.pub"
 # Temporary cloud-init.yaml file
-CL_INIT="/tmp/$(basename $0 .sh).yaml"
+CL_INIT="/tmp/$(basename "$0" .sh).yaml"
+# we use axel to download cloud image
 [ ! -x /usr/bin/axel ] && echo "Error: Please install axel package" && exit
 
 supported_os() {
 	# ============================================================================
+	# Supported cloud image
 	# local OS=$1
 	# NJU and SJTU mirror to download cloud images
 	NJU="https://mirrors.nju.edu.cn"
@@ -53,13 +62,18 @@ supported_os() {
 	# username is cloud-user, not centos
 	local centos=$NJU"/centos-cloud/centos/9-stream/x86_64/images/CentOS-Stream-GenericCloud-9-20230501.0.x86_64.qcow2"
 	# need to find a proper CN mirror for debian
-	# local debian="https://cdimage.debian.org/cdimage/cloud/bookworm/daily/latest/debian-12-generic-arm64-daily.qcow2"
 	local debian="https://cdimage.debian.org/cdimage/cloud/bookworm/daily/latest/debian-12-generic-amd64-daily.qcow2"
-	# Failed test:
-	local openeuler=$NJU"/openeuler/openEuler-23.03/virtual_machine_img/x86_64/openEuler-23.03-x86_64.qcow2.xz"
+	# local debian="https://cdimage.debian.org/cdimage/cloud/bookworm/daily/latest/debian-12-generic-arm64-daily.qcow2"
+	# local debian="https://cdimage.debian.org/cdimage/cloud/buster/daily/latest/debian-10-generic-amd64-daily.qcow2"
+	# local debian=$SJTU"/debian-cdimage/cloud/buster/latest/debian-10-generic-amd64.qcow2"
+	# username/password: root/openEuler12#$
+	local openeuler=$NJU"/openeuler/openEuler-23.09/virtual_machine_img/x86_64/openEuler-23.09-x86_64.qcow2.xz"
+	# username is anuser, password: anolisos
+	local anolis_rh=$NJU"/anolis/8.9/isos/GA/x86_64/AnolisOS-8.9-x86_64-RHCK.qcow2"
+	local anolis_an=$NJU"/anolis/8.9/isos/GA/x86_64/AnolisOS-8.9-x86_64-ANCK.qcow2"
 	#
 	[ -n "${!IMG}" ] && URL=${!IMG} && return
-	echo "Error: Supported-OS: arch,almalinux,centos,debian,fedora,jammy,openeuler,rocky"
+	echo "Error: Supported-OS: arch,almalinux,centos,debian,fedora,jammy,rocky,openeuler,anolis_rh,anolis_an"
 	echo "Syntax: $0 VM-NAME Supported-OS"
 	exit 1
 }
@@ -67,10 +81,10 @@ supported_os() {
 # Normally you don't need to change code below
 # Though function gen_yaml need to elaborate based on different OS_Type
 #
-[ ! -r $PUB_KEY ] && echo "Error: $PUB_KEY not found." && exit 1
+[ ! -r "$PUB_KEY" ] && echo "Error: $PUB_KEY not found." && exit 1
 #
-[ ! -d $BASE_DIR ] && mkdir -p $BASE_DIR
-[ ! -d $VM_DIR ] && mkdir -p $VM_DIR
+[ ! -d "$BASE_DIR" ] && mkdir -p $BASE_DIR
+[ ! -d "$VM_DIR" ] && mkdir -p $VM_DIR
 
 net_install() {
 	# Debian URL: e.g IMG=https://mirror.nju.edu.cn/debian/dists/stable/main/installer-amd64/
@@ -80,16 +94,16 @@ net_install() {
 		--graphics vnc \
 		--vcpu $CPU \
 		--memory $MEM \
-		--name $NAME \
+		--name "$NAME" \
 		--network default \
-		--disk size=$DISK_SIZE \
+		--disk size="$DISK_SIZE" \
 		--osinfo detect=on,name=generic \
-		--location $IMG
+		--location "$IMG"
 }
 
 gen_yaml() {
 	# Part A
-	cat >$CL_INIT <<EOA
+	cat >"$CL_INIT" <<EOA
 #cloud-config
 # vim: syntax=yaml
 #
@@ -112,12 +126,12 @@ local-hostname: $NAME
 ssh_authorized_keys:
 EOA
 	# Add pub key
-	echo "  - $(cat $PUB_KEY)" >>$CL_INIT
+	echo "  - $(cat $PUB_KEY)" >>"$CL_INIT"
 	# Part B
 	# - language-pack-zh-hans-base
 	# - language-pack-zh-hans
 	# byobu_by_default: disable-system
-	cat >>$CL_INIT <<EOB
+	cat >>"$CL_INIT" <<EOB
 package_update: false
 package_upgrade: false
 package_reboot_if_required: false
@@ -150,7 +164,7 @@ runcmd:
 EOB
 
 	# Final part
-	cat >>$CL_INIT <<EOF
+	cat >>"$CL_INIT" <<EOF
 final_message: "The system is finally up, after \$UPTIME seconds"
 EOF
 }
@@ -159,38 +173,46 @@ iso() {
 	/usr/bin/virt-install -d \
 		--vcpu $CPU \
 		--memory $MEM \
-		--name $NAME \
-		--disk size=$DISK_SIZE \
+		--name "$NAME" \
+		--disk size="$DISK_SIZE" \
 		--osinfo detect=on,name=generic \
 		--network default \
-		--cdrom $IMG
+		--cdrom "$IMG"
 }
 
 cloud() {
 	# Todo: check the image date, if 30 days old, still download
 	shopt -s nullglob
-	BASE=$(ls $BASE_DIR/*.qcow2 $BASE_DIR/*.img $BASE_DIR/*.raw | grep -i ^$BASE_DIR/$IMG | tail -1)
-	# echo "base:$BASE Img:$IMG"
+	BASE=$(find $BASE_DIR \( -name "$IMG*.qcow2" -o -name "$IMG*.img" -o -name "$IMG*.raw" \) -printf "%A@ %p\n" | sort | tail -1 | cut -d " " -f2)
 	# If image not exist
+	# Todo: if multiple version found, ask to choose.
 	if [ -n "$BASE" ]; then
 		echo "Info: Base image $BASE found."
 	else
-		supported_os $IMG
-		echo "Info: Downloading cloud-image from ${URL} to $BASE_DIR"
-		axel ${URL} -o $BASE_DIR
-		[ $? != 0 ] && echo "Error: failed to download ${URL}" && exit 5
-		BASE=$BASE_DIR/$(basename $URL)
+		supported_os "$IMG"
+		# echo "base:$BASE Img:$IMG"
+		BASE=$BASE_DIR/$(basename "$URL")
+		[ "$(basename "$BASE" .xz)" != "$(basename "$BASE")" ] && BASE="$BASE_DIR"/$(basename "$BASE" .xz)
+		if [ ! -r "$BASE" ]; then
+			echo "Info: Downloading cloud-image from ${URL} as $BASE ..."
+			if ! axel "$URL" -o "$BASE_DIR"; then
+				echo "Error: failed to download ${URL}"
+				exit 5
+			fi
+			# Decompress xz file and delete .xz file
+			[ "$(basename "$URL" .xz)" != "$(basename "$URL")" ] && echo "Info: Decompressing $BASE ..." && xz -v -d "$BASE".xz
+		fi
 	fi
 	# [ -z "$BASE" ] && echo "Error: No Image found for $IMG under $BASE_DIR" && exit 6
-	# Decompress xz file
-	[ "$(basename $BASE .xz)" != "$(basename $BASE)" ] && xz -v -d $BASE && BASE=$BASE_DIR/$(basename $BASE .xz)
-	FORMAT=$(qemu-img info --output json $BASE | jq -r .format)
-	[ $? != 0 ] && echo "Error: Wrong format detected for $BASE" && exit 7
+	FORMAT=$(qemu-img info --output json "$BASE" | jq -r .format)
+	[ -z "$FORMAT" ] && echo "Error: Wrong format detected for $BASE" && exit 7
 	# the Output VM Disk File Name
 	DISK=$VM_DIR/$NAME.$FORMAT
+	# disk size must greater than base image size
+	BASE_SIZE=$(qemu-img info --output json "$BASE" | jq '."virtual-size"')
 	#
-	# sudo qemu-img resize $DISK ${DISK_SIZE}G
-	sudo qemu-img create -b $BASE -F $FORMAT -f qcow2 $DISK ${DISK_SIZE}G
+	sudo qemu-img create -b "$BASE" -F "$FORMAT" -f qcow2 "$DISK" "$BASE_SIZE"
+	# sudo qemu-img resize "$DISK" "$BASE_SIZE"
 	install
 }
 
@@ -198,29 +220,36 @@ install() {
 	sudo virt-install \
 		--vcpu $CPU \
 		--memory $MEM \
-		--name $NAME \
+		--name "$NAME" \
 		--osinfo detect=on,name=generic \
 		--network default \
 		--noautoconsole \
-		--import --disk $DISK,bus=virtio \
-		--cloud-init user-data=$CL_INIT
+		--import --disk "$DISK",bus=virtio \
+		--cloud-init user-data="$CL_INIT"
 	# --cloud-init root-password-generate=on,disable=on,ssh-key=/home/xxx/.ssh/id_rsa.pub
 	# clouduser-ssh-key=$PUB_KEY,root-ssh-key=$PUB_KEY
 	# network-config
 }
 
 # Main Prog.
-if [ "$(echo $IMG | cut -c1-4)" = "http" ]; then
+# If begin with http, then run net_install
+if [ "$(echo "$IMG" | cut -c1-4)" = "http" ]; then
 	net_install
 	exit 0
 fi
-if [ "$(basename $IMG .iso).iso" = "$(basename $IMG)" ]; then
+# if IMG is .iso, then run iso
+if [ "$(basename "$IMG" .iso).iso" = "$(basename "$IMG")" ]; then
 	echo "Install from Live CD: $IMG"
 	iso
 else
 	echo "Import qcow2 disk: $IMG"
-	[ -r $IMG ] && DISK=$IMG
-	[ -r $BASE_DIR/$IMG ] && DISK=$BASE_DIR/$IMG
+	[ -r "$IMG" ] && DISK="$IMG"
+	[ -r $BASE_DIR/"$IMG" ] && DISK=$BASE_DIR/$IMG
 	gen_yaml
-	[ -n "$DISK" ] && install || cloud
+	# if DISK is null, then run import qcow2 from cloud
+	if [ -n "$DISK" ]; then
+		install
+	else
+		cloud
+	fi
 fi
