960f10a81ca099c6271474b3c01237881828ea84
Albert Xu <albertxu@freelamp.com>
2024-04-16 05:37:57 +0800

    revised to stop use tmp_file

--- scripts/dman.sh
+++ scripts/dman.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-# TL'DR: My own dman, grab man page from debian.org
-# Ver 0.1.1
+# TL'DR: My own dman, grab man page from manpages.debian.org
+# Ver 0.2.1
 # Author: Bilibili ID: LeisureLinux
 # Last Modified: 2024年 04月 14日 星期日 21:26:23 CST
 #
 set -e
-PROG=$1
-[ -z "$PROG" ] && echo "Syntax: $0 prog_name" && exit 0
-tmp_file="/tmp/dman.$PROG.$$"
+[ -z "$1" ] && echo "Syntax: $0 prog_name" && exit 0
 BASE_URL="https://manpages.debian.org"
-CURL_OPT="-sSL"
-raw=$(curl $CURL_OPT "$BASE_URL/$PROG" | awk -F'"' '/raw man page/ {print $2}')
-[ -z "$raw" ] && echo "Error: raw man page for $PROG not found!" && exit 1
-curl -sSf "$BASE_URL"/"$raw" -o "$tmp_file"
-man -l "$tmp_file" && rm "$tmp_file"
+raw=$(curl -sSL "$BASE_URL/$1" | awk -F'"' '/raw man page/ {print $2}')
+[ -z "$raw" ] && echo "Error: raw man page for $1 not found!" && exit 1
+curl -sSf "$BASE_URL"/"$raw" -o - | man -l -
